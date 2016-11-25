8dbaf10884a737e9ac6d4c33dfd15c2c66978bf7
zhangwei <719048757@qq.com>
2016-11-26 00:45:42 +0800

    d

--- db/migrate/20161125163201_create_accounts_accounts.rb
+++ db/migrate/20161125163201_create_accounts_accounts.rb
@@ -1,6 +1,6 @@
-class CreateAccounts < ActiveRecord::Migration[5.0]
+class CreateAccountsAccounts < ActiveRecord::Migration[5.0]
   def change
-    create_table :accounts do |t|
+    create_table :accounts_accounts do |t|
       t.string :uuid
       t.timestamps
     end
