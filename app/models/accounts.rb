0eb26b258459e8f40fcb864e6059dd142cf52010
zhangwei <719048757@qq.com>
2016-11-26 00:43:16 +0800

    新建accout表字段

--- app/models/accounts.rb
+++
@@ -1,5 +0,0 @@
-module Accounts
-  def self.table_name_prefix
-    'accounts_'
-  end
-end
--- db/migrate/20161125163201_create_accounts_accounts.rb
+++ db/migrate/20161125163201_create_accounts_accounts.rb
@@ -1,7 +1,7 @@
-class CreateAccountsAccounts < ActiveRecord::Migration[5.0]
+class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
-    create_table :accounts_accounts do |t|
-
+    create_table :accounts do |t|
+      t.string :uuid
       t.timestamps
     end
   end
