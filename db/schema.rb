361a78542bd3390d7cef6b30e6827fb79eeb1157
zhangwei <719048757@qq.com>
2016-11-26 15:21:58 +0800

    新建数据库表

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125163201) do
+ActiveRecord::Schema.define(version: 20161125164259) do
+
+  create_table "accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "uuid",          limit: 32
+    t.string   "mobile_number", limit: 50
+    t.string   "email",         limit: 50
+    t.string   "password",      limit: 32
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "accounts_accounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "uuid"
